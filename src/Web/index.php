8ca6cd9411673196e8ccfa43df158ccb4a822a98
Lars Lauger <gh@lars.wtf>
2022-02-14 20:43:23 +0100

    BUGFIX: Only handle webhooks when request is POST

--- src/Web/index.php
+++ src/Web/index.php
@@ -6,6 +6,10 @@ require_once(__DIR__ . '/../../vendor/autoload.php');
 use Lala\Zemusibot\Bot\Bot;
 use Longman\TelegramBot\Exception\TelegramException;
 
+if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+    exit(0);
+}
+
 $bot = Bot::createFromEnvironment();
 
 try {
